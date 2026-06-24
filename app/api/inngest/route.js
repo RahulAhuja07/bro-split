16cce3e4934d8cf97d36e6ee08efbca5fe0bec49
RahulAhuja07 <ahujarahul906@gmail.com>
2026-06-24 14:01:27 +0530

    fixed all the errors and bugs and also started the feature of mails sending

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,7 +1,10 @@
 import { inngest } from "@/lib/inngest/client";
+import { paymentReminders } from "@/lib/inngest/payment-reminders";
 import { serve } from "inngest/next";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [],
+  functions: [
+    paymentReminders
+  ],
 });
